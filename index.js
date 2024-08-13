aa62aa1d67314f295e264949afa2eedd02a6694b
nadarsurya1357@gmail.com <nadarsurya1357@gmail.com>
2024-08-13 18:37:10 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const { users } = require("./Data/user.json");
+
 const app = express();
 
 const PORT = 8081;
@@ -9,6 +11,21 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "Server is up and running :-)",
+  });
+});
+
+/**
+ * Route : /user
+ * Method : GET
+ * Description : Get all the users
+ * Access : Public
+ * Parameters : None
+ */
+
+app.get("/users", (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: users,
   });
 });
 
